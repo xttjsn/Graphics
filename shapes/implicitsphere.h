03a832eb02312cea54c555c22a41ba89ffce7ec7
Jason Tang <xttjsn@gmail.com>
2018-10-31 18:06:21 -0400

    Implement diffuseAtIntersect in ImplicitShape

--- shapes/implicitsphere.h
+++ shapes/implicitsphere.h
@@ -9,7 +9,6 @@ public:
 
     Intersect intersect(const Ray& ray);
     glm::vec4 normal(Intersect& intersect);
-    glm::vec4 diffuseAtIntersect(Intersect& intersect, CS123SceneLightData& light, CS123SceneGlobalData& global);
     float surfaceArea();
     BoundingBox boundingBox();
 };
